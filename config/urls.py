f129ae89ee912dd865b3c54373ebba2de8b41213
bmarika <marika@savannahinformatics.com>
2015-03-26 14:59:54 +0300

    Clean up

--- config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns
-
-
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mfl_api.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-)
