d783263c0d0885ad1cceb037b04ef5ab22b43819
bmarika <marika@savannahinformatics.com>
2015-03-27 10:12:21 +0300

    Add stubs for facility models.

---
+++ facilities/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class service(models.Model):
+    pass
+
+class Facility(models.Model):
+    """
+    name
+    District 
+    """
+
+    
+class FacilityServices(models.Model):
+    pass
