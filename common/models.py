17dd332df0b2f28e2e6665b9e3614921d3928027
bmarika <marika@savannahinformatics.com>
2015-03-27 10:11:58 +0300

    Add common services to thier own app.

---
+++ common/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class County(models.Model):
+    name = models.Char
+    pass
+
+
+class District(models.Model):
+    pass
+
+
+class Division(models.Model):
+    pass
