a43aa6dedba6240ffc624af8d5eb081089a06dc3
bmarika <marika@savannahinformatics.com>
2015-04-08 14:53:52 +0300

    Rename GIS model to FacilityGIS Also added the facillity field as a OneToOne to facility

--- facilities/models.py
+++ facilities/models.py
@@ -34,12 +34,6 @@ class Service(AbstractBase):
         super(Service, self).save(*args, **kwargs)
 
 
-class GIS(AbstractBase):
-    latitude = models.CharField(max_length=255)
-    longitude = models.CharField(max_length=255)
-    is_classified = models.BooleanField(default=False)
-
-
 class FacilityStatus(AbstractBase):
     name = models.CharField(max_length=100)
 
@@ -51,7 +45,6 @@ class FacilityType(AbstractBase):
 class Facility(AbstractBase):
     name = models.CharField(max_length=100, unique=True)
     code = models.CharField(max_length=100, unique=True)
-    gis = models.OneToOneField(GIS, null=True, blank=True)
     description = models.TextField()
     facility_type = models.OneToOneField(FacilityType)
     services = models.ManyToManyField(Service)
@@ -81,3 +74,10 @@ class Facility(AbstractBase):
         if not self.code:
             self.code = self.generate_code()
         super(Service, self).save(*args, **kwargs)
+
+
+class FacitlityGIS(AbstractBase):
+    gis = models.OneToOneField(Facility)
+    latitude = models.CharField(max_length=255)
+    longitude = models.CharField(max_length=255)
+    is_classified = models.BooleanField(default=False)
