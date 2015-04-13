51683bee7cfbefe6b7d42fd49e0ceb862aaf55d6
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-04-13 14:19:59 +0300

    Added a DEPLOYING.rst file

--- fabfile.py
+++ fabfile.py
@@ -16,3 +16,10 @@ def test():
 
 def run():
     local('{}/manage.py runserver 8000'.format(BASE_DIR))
+
+
+def deploy():
+    """
+    Should be run only by the release manager
+    """
+    local('python setup.py sdist upload -r slade')
