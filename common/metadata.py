fea2f2760a41d0bf41ad218fc3d64ee5fc4cb064
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-04-15 15:30:15 +0300

    API Root now displays metadata for list endpoints

--- common/metadata.py
+++ common/metadata.py
@@ -1,9 +1,9 @@
 from __future__ import unicode_literals
 
-from rest_framework.metadata import BaseMetadata
+from rest_framework.metadata import SimpleMetadata
 
 
-class CustomMetadata(BaseMetadata):
+class CustomMetadata(SimpleMetadata):
     """
     Based on the implementation of SimpleMetaData in DRF v3.1
     """
--- common/views/shared_views.py
+++ common/views/shared_views.py
@@ -1,63 +1,85 @@
+from django.core.urlresolvers import resolve
+from django.core.urlresolvers import reverse as django_reverse
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
+from ..metadata import CustomMetadata
+
+
+MODEL_VIEW_DICT = {
+    'counties': {
+        'list_url_name': 'api:common:counties_list'
+    },
+    'users': {
+        'list_url_name': 'api:users:users_list'
+    },
+    'facilities': {
+        'list_url_name': 'api:facilities:facility_list'
+    },
+    'contacts': {
+        'list_url_name': 'api:common:contacts_list'
+    },
+    'contact_types': {
+        'list_url_name': 'api:common:contact_types_list'
+    },
+    'wards': {
+        'list_url_name': 'api:common:wards_list'
+    },
+    'constituencies': {
+        'list_url_name': 'api:common:constituencies_list'
+    },
+    'owners': {
+        'list_url_name': 'api:facilities:owners_list'
+    },
+    'owner_types': {
+        'list_url_name': 'api:facilities:facility_list'
+    },
+    'services': {
+        'list_url_name': 'api:facilities:services_list'
+    }
+}
+METADATA_CLASS = CustomMetadata()
+
+
+def _reverse(request, url_name):
+    return reverse(
+        url_name,
+        request=request
+    )
+
+
+def _resolve_metadata(request, url_name):
+    url_path = django_reverse(url_name)
+    view = resolve(url_path).func.cls()
+    view.initial(request)
+    return METADATA_CLASS.determine_metadata(request, view)
+
+
+def _lookup_metadata(url_name_dict, request):
+    return {
+        'list_endpoint':
+            _reverse(request, url_name_dict['list_url_name']),
+        'list_metadata':
+            _resolve_metadata(
+                request,
+                url_name_dict['list_url_name']
+            )
+    }
+
 
 class APIRoot(APIView):
+    """
+    This view serves as the entry point to the entire API
+    """
 
     def get(self, request, format=None):
         return Response({
-            'counties': reverse(
-                'api:common:counties_list',
-                request=request,
-                format=format
-            ),
-            'users': reverse(
-                'api:users:users_list', request=request, format=format
-            ),
-            'facilities': reverse(
-                'api:facilities:facility_list',
-                request=request,
-                format=format
-            ),
-            'contacts': reverse(
-                'api:common:contacts_list',
-                request=request,
-                format=format
-            ),
-            'contact_types': reverse(
-                'api:common:contact_types_list',
-                request=request,
-                format=format
-            ),
-            'wards': reverse(
-                'api:common:wards_list',
-                request=request,
-                format=format
-            ),
-            'constituencies': reverse(
-                'api:common:constituencies_list',
-                request=request,
-                format=format
-            ),
-            'owners': reverse(
-                'api:facilities:owners_list',
-                request=request,
-                format=format
-            ),
-            'owner_types': reverse(
-                'api:facilities:facility_list',
-                request=request,
-                format=format
-            ),
-            'services': reverse(
-                'api:facilities:services_list',
-                request=request,
-                format=format
-            )
+            model_type_name: _lookup_metadata(url_name_dict, request)
+            for model_type_name, url_name_dict in MODEL_VIEW_DICT.iteritems()
         })
 
-# TODO Do the root API view in class based view style
 # TODO Introspect and reverse all list URLs
 #    ( should be easy to introspect views from all apps )
 # TODO Retrieve metadata for each of those URLs and inline it for lists
