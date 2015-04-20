a23cb67cd7e24109d2656ff9e85aa96d785c6604
bmarika <marika@savannahinformatics.com>
2015-04-20 19:02:49 +0300

    Checkpoint

--- chul/models.py
+++ chul/models.py
@@ -1,9 +1,36 @@
 from django.db import models
-from common.models import AbstractBase, Contact
+from common.models import AbstractBase, Contact, SequenceMixin, Ward
+from common.fields import SequenceField
 from facilities.models import Facility
 
 
-class CommunityHealthUnit(AbstractBase):
+class Status(AbstractBase):
+    """
+    Indicates the of operation of a community health unit.
+    e.g  fully functiona, semi operational, operational
+    """
+    name = models.CharField(max_length=50)
+    description = models.TextField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Community(SequenceMixin, AbstractBase):
+    """
+    A certain area within a ward.
+    """
+    name = models.CharField(max_length=100)
+    code = SequenceField(unique=True)
+    ward = models.ForeignKey(Ward)
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = self.generate_next_code_sequence()
+        super(Community, self).save(*args, **kwargs)
+
+
+class CommunityHealthUnit(SequenceMixin, AbstractBase):
     """
     This is a health service delivery structure within a defined geographical
     area covering a population of approximately 5,000 people.
@@ -15,12 +42,22 @@ class CommunityHealthUnit(AbstractBase):
     name = models.CharField(
         max_length=100,
         help_text="")
+    code = SequenceField(unique=True)
     facility = models.ForeignKey(
         Facility,
         help_text='The facility on which the health unit is tied to.')
+    status = models.ForeignKey(Status)
+    community = models.ForeignKey(
+        Community,
+        help_text='Community area within which the health unit is located')
 
     def __unicode__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = self.generate_next_code_sequence()
+        super(CommunityHealthUnit, self).save(*args, **kwargs)
 
 
 class CommunityHealthWorker(AbstractBase):
@@ -31,6 +68,7 @@ class CommunityHealthWorker(AbstractBase):
     last_name = models.CharField(max_length=50)
     surname = models.CharField(max_length=50)
     id_number = models.PositiveIntegerField(unique=True)
+    households_monitored = models.PositiveIntegerField(defaul=0)
 
     def __unicode__(self):
         return str(self.id_number)
