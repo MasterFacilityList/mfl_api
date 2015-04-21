737fe9dba4ad40929204b80652e521613304878a
bmarika <marika@savannahinformatics.com>
2015-04-21 17:22:07 +0300

    Update migrations.

--- facilities/models/facility_models.py
+++ facilities/models/facility_models.py
@@ -552,7 +552,7 @@ class Service(SequenceMixin, AbstractBase):
     name = models.CharField(max_length=255, unique=True)
     description = models.TextField(null=True, blank=True)
     abbreviation = models.CharField(
-        null=True, blank=True,
+        max_length=50, null=True, blank=True,
         help_text='A short form for the service e.g FANC for Focused '
         'Antenatal Care')
     category = models.ForeignKey(
