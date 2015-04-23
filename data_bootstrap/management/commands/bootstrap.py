f6a1e82930ff367255b73ddecace47f9267a606d
bmarika <marika@savannahinformatics.com>
2015-04-23 14:02:17 +0300

    Checkpoint.

--- data_bootstrap/management/commands/bootstrap.py
+++ data_bootstrap/management/commands/bootstrap.py
@@ -1,20 +1,37 @@
 import os
 import glob
 
+from django.conf import settings
+
 from django.core.management import BaseCommand
 
-from sil_data_bootstrap.bootstrap import process_json_file
+from ...bootstrap import process_json_file
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
+        counties = os.path.join(settings.BASE_DIR, 'data/data/counties.json')
+        constituencies = os.path.join(
+            settings.BASE_DIR, 'data/data/constituencies.json')
+        wards = os.path.join(settings.BASE_DIR, 'data/data/wards.json')
+        owners = os.path.join(
+            settings.BASE_DIR, 'data/data/facility_owners.json')
+
+        owners = os.path.join(
+            settings.BASE_DIR, 'data/data/service_categories.json')
+        args = [counties, constituencies, wards, owners]
+
         for suggestion in args:
             # if it's just a simple json file
-            if os.path.exists(suggestion) and os.path.isfile(suggestion):
-                process_json_file(suggestion)
-            else:
-                # check if it's a glob
-                for filename in glob.glob(suggestion):
-                    process_json_file(filename)
+            try:
+                if os.path.exists(suggestion) and os.path.isfile(suggestion):
+                    process_json_file(suggestion)
+                else:
+                    # check if it's a glob
+                    for filename in glob.glob(suggestion):
+                        process_json_file(filename)
+            except:
+                pass
+
         self.stdout.write("Done loading")
