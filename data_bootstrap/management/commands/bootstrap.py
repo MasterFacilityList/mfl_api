e2b347bcbc55eb28bac17faab1a751686f267770
bmarika <marika@savannahinformatics.com>
2015-04-24 09:58:05 +0300

    Add job titles data

--- data_bootstrap/management/commands/bootstrap.py
+++ data_bootstrap/management/commands/bootstrap.py
@@ -14,24 +14,25 @@ class Command(BaseCommand):
         counties = os.path.join(settings.BASE_DIR, 'data/data/counties.json')
         constituencies = os.path.join(
             settings.BASE_DIR, 'data/data/constituencies.json')
-        wards = os.path.join(settings.BASE_DIR, 'data/data/wards.json')
+        #  wards = os.path.join(settings.BASE_DIR, 'data/data/wards.json')
         owners = os.path.join(
             settings.BASE_DIR, 'data/data/facility_owners.json')
 
         owners = os.path.join(
             settings.BASE_DIR, 'data/data/service_categories.json')
-        args = [counties, constituencies, wards, owners]
+        job_titles = os.path.join(
+            settings.BASE_DIR, 'data/data/job_titles.json')
+        args = [
+            counties, constituencies, owners, job_titles
+        ]
 
         for suggestion in args:
             # if it's just a simple json file
-            try:
-                if os.path.exists(suggestion) and os.path.isfile(suggestion):
-                    process_json_file(suggestion)
-                else:
-                    # check if it's a glob
-                    for filename in glob.glob(suggestion):
-                        process_json_file(filename)
-            except:
-                pass
+            if os.path.exists(suggestion) and os.path.isfile(suggestion):
+                process_json_file(suggestion)
+            else:
+                # check if it's a glob
+                for filename in glob.glob(suggestion):
+                    process_json_file(filename)
 
         self.stdout.write("Done loading")
