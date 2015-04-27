1ab663cc041cf4530b7644949673fac745b21e2b
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-04-27 22:58:08 +0300

    Stubbed out tests for existing management commands

---
+++ mfl_gis/tests/test_data_load_management_commands.py
@@ -0,0 +1,42 @@
+from common.tests.test_models import BaseTestCase
+
+
+class TestLoadConstituencyBoundaries(BaseTestCase):
+    def test_no_constituency(self):
+        pass
+
+    def test_existing_constituency_no_boundary(self):
+        pass
+
+    def test_existing_constituency_boundary(self):
+        pass
+
+
+class TestLoadWardBoundaries(BaseTestCase):
+    def test_no_ward(self):
+        pass
+
+    def test_existing_ward_no_boundary(self):
+        pass
+
+    def test_existing_ward_boundary(self):
+        pass
+
+
+class TestLoadCountyBoundaries(BaseTestCase):
+    def test_no_county(self):
+        pass
+
+    def test_existing_county_no_boundary(self):
+        pass
+
+    def test_existing_county_boundary(self):
+        pass
+
+
+class TestLoadWorldBoundaries(BaseTestCase):
+    def test_existing_countries(self):
+        pass
+
+    def test_no_existing_countries(self):
+        pass
