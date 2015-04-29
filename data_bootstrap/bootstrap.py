b50b4d510eaa7b2e25f4775801f7d1df4ebc0ec6
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-04-29 13:02:55 +0300

    Checkpoint: before shortening the custom SequenceField
    It should end up as a lightly customized version of the AutoField

--- data_bootstrap/bootstrap.py
+++ data_bootstrap/bootstrap.py
@@ -1,19 +1,16 @@
 import json
 import logging
 
-from functools import partial
+from collections import defaultdict
 from django.db.models import get_model
+from django.db import transaction
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-def get_model_cls(model):
-    app, model_name = model.split('.', 1)  # split only once
-    return get_model(app, model_name)
-
-
-def resolve_foreign_key(model_cls, field_data):
+def _retrieve_existing_model_instance(model_cls, field_data):
+    """RETRIEVE an existing model instance ( mainly used to resolve FKs )"""
     # to prevent any modifications making their way back to
     # the original dict
     assert isinstance(field_data, dict)
@@ -21,20 +18,25 @@ def resolve_foreign_key(model_cls, field_data):
         instance = model_cls.objects.get(**field_data)
     except:
         LOGGER.error(
-            'Unable to get an instance of {} with {}'
+            'Unable to get an instance of {} with attributes {}'
             .format(model_cls, field_data)
         )
         raise
     return instance
 
 
-def resolve_foreign_keys(model_cls, record):
+def _resolve_foreign_keys(model_cls, record):
+    """Retrieve and link instances of models referred to by FK
+
+    This is the one step that imposes a dependency order to the data load.
+    The instance that is referred to must already exist.
+    """
     new_record = {}
     for field in record.keys():
         field_data = record[field]
         model_field = model_cls._meta.get_field(field)
         if model_field.get_internal_type() == "ForeignKey":
-            new_record[field] = resolve_foreign_key(
+            new_record[field] = _retrieve_existing_model_instance(
                 model_field.rel.to, field_data)
         else:
             new_record[field] = field_data
@@ -42,8 +44,10 @@ def resolve_foreign_keys(model_cls, record):
     return new_record
 
 
-def process_record(model, unique_fields, record):
-    model_cls = get_model_cls(model)
+def _instantiate_single_record(model, unique_fields, record):
+    """Create unsaved model instances, ready to be sent to bulk_create"""
+    app, model_name = model.split('.', 1)  # split only once
+    model_cls = get_model(app, model_name)
 
     if unique_fields:
         unique_dict = {}
@@ -52,39 +56,63 @@ def process_record(model, unique_fields, record):
             field_data = record[field]
             if model_field.get_internal_type() == "ForeignKey":
                 field_data = field_data.copy()
-                instance = resolve_foreign_key(
+                instance = _retrieve_existing_model_instance(
                     model_field.rel.to, field_data)
                 unique_dict[field] = instance
             else:
                 unique_dict[field] = field_data
 
         try:
-            model_cls.objects.get(**unique_dict)
+            instance = model_cls.objects.get(**unique_dict)
+            LOGGER.debug(
+                'Model with attributes "{}" already exists: {}'
+                .format(unique_dict, instance)
+            )
+            return model_cls, None  # Must test for None in calling code
         except model_cls.DoesNotExist:
-            pass
-        else:
-            # record is already in the database so don't add
-            return
+            try:
+                LOGGER.debug(
+                    'Model with attributes "{}" does not exist, making one'
+                    .format(unique_dict)
+                )
+                normalized_record = _resolve_foreign_keys(model_cls, record)
+                return model_cls, model_cls(**normalized_record)
+            except Exception as e:  # Don't panic, we will be re-raising
+                LOGGER.error(
+                    '"{}" when instantiating a record of "{}" with unique '
+                    'fields "{}" and data "{}"'
+                    .format(e, model, unique_fields, record)
+                )
+                raise
+    else:
+        LOGGER.error('Data file error; unique fields not specified')
 
-    normalized_record = resolve_foreign_keys(model_cls, record)
-    model_cls.objects.create(**normalized_record)
 
-
-def process_model_spec(model_spec):
+def _process_model_spec(model_spec):
+    """For each model spec, instantiate but do not save ( bulk save later )"""
     model = model_spec['model']
     unique_fields = model_spec.get('unique_fields', [])
     records = model_spec['records']
 
-    temp_func = partial(process_record, model, unique_fields)
-    map(temp_func, records)
+    # The first version of this function used some fancy functional techniques
+    # ( partials / currying ); we go back to a simpler, more deterministic way
+    unsaved_instances = defaultdict(list)
+    for record in records:
+        model_cls, unsaved_obj = _instantiate_single_record(
+            model, unique_fields, record)
+        if unsaved_obj:  # For existing instances, obj is set to `None`
+            unsaved_instances[model_cls].append(unsaved_obj)
 
-
-def process_data_list(data):
-    map(process_model_spec, data)
+    for model_cls, instances in unsaved_instances.iteritems():
+        with transaction.atomic():
+            model_cls.objects.bulk_create(instances)
+            LOGGER.debug(
+                'Created {} instances of {}'.format(len(instances), model_cls))
 
 
 def process_json_file(filename):
+    """The entry point - loops through data files and loads each in"""
+    LOGGER.debug('Processing {}'.format(filename))
     with open(filename) as f:
-        data = f.read()
-        json_data = json.loads(data)
-        process_data_list(json_data)
+        model_specs = json.load(f)
+        map(_process_model_spec, model_specs)
