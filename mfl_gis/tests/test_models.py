14548a0295afe90b7b9036a02e0e70ad7d2ff246
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-04-29 09:20:00 +0300

    Added pytest-xdist to dependencies

--- mfl_gis/tests/test_models.py
+++ mfl_gis/tests/test_models.py
@@ -1,4 +1,6 @@
 from model_mommy import mommy
+from django.core.exceptions import ValidationError
+from django.contrib.gis.geos import Point
 from common.tests.test_models import BaseTestCase
 
 from ..models import GeoCodeSource, GeoCodeMethod, FacilityCoordinates
@@ -46,26 +48,34 @@ class TestFacilityCoordinatesModel(BaseTestCase):
         self.assertEquals(
             facility_gps.facility.name, facility_gps.__unicode__())
 
-    def test_validate_longitude_and_latitude_within_kenya_valid(self):
-        pass
-
     def test_validate_longitude_and_latitude_within_kenya_invalid(self):
-        pass
-
-    def test_validate_longitude_and_latitude_within_county_valid(self):
-        pass
+        """The Kampala Serena - 0.319590, 32.586484; definitely not in Kenya"""
+        with self.assertRaises(ValidationError):
+            mommy.make_recipe(
+                'mfl_gis.tests.facility_coordinates_recipe',
+                coordinates=Point(32.586484, 0.319590)
+            )
 
     def test_validate_longitude_and_latitude_within_county_invalid(self):
-        pass
-
-    def test_validate_longitude_and_latitude_within_constituency_valid(self):
-        pass
+        """Thomson Falls - 0.044444, 36.370416 - is in Kenya but not Nairobi"""
+        with self.assertRaises(ValidationError):
+            mommy.make_recipe(
+                'mfl_gis.tests.facility_coordinates_recipe',
+                coordinates=Point(36.370416, 0.044444)
+            )
 
     def test_validate_longitude_and_latitude_within_constituency_invalid(self):
-        pass
-
-    def test_validate_longitude_and_latitude_within_ward_valid(self):
-        pass
+        """Taj Mall (-1.323139, 36.898769) is in Nrb but not Dagoretti North"""
+        with self.assertRaises(ValidationError):
+            mommy.make_recipe(
+                'mfl_gis.tests.facility_coordinates_recipe',
+                coordinates=Point(36.898769, -1.323139)
+            )
 
     def test_validate_longitude_and_latitude_within_ward_invalid(self):
-        pass
+        """SIL - -1.300470, 36.791655 - is just outside Kilimani ward"""
+        with self.assertRaises(ValidationError):
+            mommy.make_recipe(
+                'mfl_gis.tests.facility_coordinates_recipe',
+                coordinates=Point(36.791655, -1.300470)
+            )
