60c5d41959835e44c0af852c8faee8925192a3e7
bmarika <marika@savannahinformatics.com>
2015-05-04 11:32:12 +0300

    Change circlehost to 127.0.0.1

--- config/settings/test.py
+++ config/settings/test.py
@@ -3,6 +3,6 @@ from .base import *  # NOQA
 
 if os.getenv('CI') == 'true':
     SEARCH = {
-        "ELASTIC_URL": "http://circlehost:9200/",
+        "ELASTIC_URL": "http://127.0.0.1:9200/",
         "INDEX_NAME": "mfl_index"
     }
