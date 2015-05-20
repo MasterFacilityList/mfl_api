ccbebb95d3dfca8d37ec4ddbaf8f7d115828cb5f
Ngure Nyaga <ngure.nyaga@savannahinformatics.com>
2015-05-20 07:39:16 +0300

    Set cache backend to ignore exceptions and to compress stored values

--- config/settings/base.py
+++ config/settings/base.py
@@ -231,6 +231,8 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "COMPRESS_MIN_LEN": 10,
+            "IGNORE_EXCEPTIONS": True,
         }
     }
 }
