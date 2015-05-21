5e5baacfb682aa86db05a0ea02612cf595b01926
bmarika <marika@savannahinformatics.com>
2015-05-21 16:31:01 +0300

    Pep 8 clean up

--- common/tests/test_excel_renderer.py
+++ common/tests/test_excel_renderer.py
@@ -1,12 +1,7 @@
-import os
-
-from django.conf import settings
-
 from django.core.urlresolvers import reverse
 from rest_framework.test import APITestCase
 from .test_views import LoginMixin
 
-from rest_framework.exceptions import ValidationError
 from common.models import County
 from model_mommy import mommy
 from common.renderers import _write_excel_file
