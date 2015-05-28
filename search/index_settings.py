ca4e65ae56093639047eede994b4904a16beb210
bmarika <marika@savannahinformatics.com>
2015-05-28 14:55:06 +0300

    Perform autocomplete searches by default

--- search/index_settings.py
+++ search/index_settings.py
@@ -1,20 +1,26 @@
 INDEX_SETTINGS = {
-  "settings":{
-    "analysis":{
-      "analyzer":{
-        "default":{
-          "type":"custom",
-          "tokenizer":"standard",
-          "filter":[ "standard", "lowercase", "stop", "kstem", "ngram" ] 
+    "settings": {
+        "analysis": {
+            "analyzer": {
+                "default": {
+                    "type": "custom",
+                    "tokenizer": "standard",
+                    "filter": [
+                        "standard",
+                        "lowercase",
+                        "stop",
+                        "kstem",
+                        "ngram"
+                    ]
+                }
+            },
+            "filter": {
+                "ngram": {
+                    "type": "ngram",
+                    "min_gram": 2,
+                    "max_gram": 15
+                }
+            }
         }
-      },
-      "filter": {
-        "ngram": {
-          "type": "ngram",
-          "min_gram": 6,
-          "max_gram": 15
-        }
-      }
     }
-  }
 }
