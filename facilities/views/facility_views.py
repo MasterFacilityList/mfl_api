53de39106ca7247c8e9c5e17338d5a518e605273
bmarika <marika@savannahinformatics.com>
2015-06-26 12:22:10 +0300

    Remove non used code

--- facilities/views/facility_views.py
+++ facilities/views/facility_views.py
@@ -217,27 +217,6 @@ class FacilityListReadOnlyView(
     )
 
 
-class UpdateModelMixin(object):
-    """
-    Update a model instance.
-    """
-    def update(self, request, *args, **kwargs):
-        partial = kwargs.pop('partial', False)
-        instance = self.get_object()
-        serializer = self.get_serializer(
-            instance, data=request.data, partial=partial)
-        serializer.is_valid(raise_exception=True)
-        self.perform_update(serializer)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def perform_update(self, serializer):
-        serializer.save()
-
-    def partial_update(self, request, *args, **kwargs):
-        kwargs['partial'] = True
-        return self.update(request, *args, **kwargs)
-
-
 class FacilityDetailView(
         QuerysetFilterMixin,
         AuditableDetailViewMixin, CustomRetrieveUpdateDestroyView):
