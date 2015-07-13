d0c2e8ba0f786715e3950ec830f3c21f6b9eac65
bmarika <marika@savannahinformatics.com>
2015-07-13 18:18:33 +0300

    House keeping

--- facilities/migrations/alter_facility_code_sequence.py
+++ facilities/migrations/alter_facility_code_sequence.py
@@ -5,9 +5,10 @@ from facilities.models import Facility
 def set_min_code_value(apps, schema_editor):
     from django.db import connection
     cursor = connection.cursor()
-    cursor = cursor.execute("""
-                             ALTER SEQUENCE facilities_facility_code_seq restart 100000 start 100000 minvalue 100000
-                            """)
+    sql = """
+          ALTER SEQUENCE facilities_facility_code_seq restart 100000 start 100000 minvalue 100000
+          """
+    cursor = cursor.execute(sql)
 
 
 class Migration(migrations.Migration):
