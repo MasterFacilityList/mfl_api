e96b24b81c8dcb8511824b15aba8d1ac4967be68
bmarika <marika@savannahinformatics.com>
2015-07-13 17:50:56 +0300

    Alter the facility code to start at 100000

---
+++ facilities/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('facilities', 'alter_facility_code_sequence'),
+        ('facilities', '0011_remove_facility_location_desc'),
+    ]
+
+    operations = [
+    ]
---
+++ facilities/migrations/alter_facility_code_sequence.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.db import models, migrations
+from facilities.models import Facility
+
+def set_min_code_value(apps, schema_editor):
+    from django.db import connection
+    cursor = connection.cursor()
+    cursor = cursor.execute("""
+                             ALTER SEQUENCE facilities_facility_code_seq restart 100000 start 100000 minvalue 100000
+                            """)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('facilities', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(set_min_code_value),
+    ]
