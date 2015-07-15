624e2109e6e6e94d4410a6d3a18b1cbf0577e243
Chomba Ng'ang'a <choeringa@gmail.com>
2015-07-15 11:00:31 +0300

    small fixes

--- facilities/views/facility_views.py
+++ facilities/views/facility_views.py
@@ -235,7 +235,7 @@ class FacilityDetailView(
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
-        request.data['updated_by'] = request.user.id
+        request.data['updated_by_id'] = str(request.user.id)
         instance = self.get_object()
         serializer = self.get_serializer(
             instance, data=request.data, partial=partial)
