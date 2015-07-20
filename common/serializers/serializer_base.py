fa6ab84a3502252b0f6b1ae073543d2e79d1ec83
Chomba Ng'ang'a <choeringa@gmail.com>
2015-07-20 09:37:57 +0300

    initial setup of partial response

--- common/serializers/serializer_base.py
+++ common/serializers/serializer_base.py
@@ -1,13 +1,14 @@
+import six
+
 from django.utils import timezone
 
 
 class AbstractFieldsMixin(object):
+
     """
     Injects the fields in the abstract base model as a model
     instance is being saved.
     """
-    def __init__(self, *args, **kwargs):
-        super(AbstractFieldsMixin, self).__init__(*args, **kwargs)
 
     def create(self, validated_data):
         """`created` and `created_by` are only mutated if they are null"""
@@ -22,3 +23,28 @@ class AbstractFieldsMixin(object):
         validated_data['updated_by'] = self.context['request'].user
 
         return self.Meta.model.objects.create(**validated_data)
+
+    def get_fields(self):
+        """
+        Fetch a subset of fields from the serializer determined by the
+        request's ``fields`` query parameter.
+
+        This is an initial implementation that does not handle:
+          - nested relationships
+          - rejection of unknown fields (currently ignoring unknown fields)
+          - wildcards
+          - e.t.c
+        """
+        origi_fields = super(AbstractFieldsMixin, self).get_fields()
+        request = self.context['request']
+        if request.method != 'GET':
+            return origi_fields
+
+        fields = request.QUERY_PARAMS.get('fields', None)
+        if isinstance(fields, six.string_types) and fields:
+            fields = fields.split(',')
+            return {
+                field: origi_fields[field]
+                for field in origi_fields if field in fields
+            }
+        return origi_fields
