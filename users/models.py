f56dfe07c4fd0197b93ca5df48742a90ca64a699
bmarika <marika@savannahinformatics.com>
2015-07-24 14:45:19 +0300

    Add a permission to allow for manipulation of superusers

--- users/models.py
+++ users/models.py
@@ -173,12 +173,21 @@ class MflUser(AbstractBaseUser, PermissionsMixin):
                 'county_group_marker',
                 'A marker permission for county level groups'
             ),
+            (
+                'manipulate_superusers',
+                'A permission to create and manipulate superusers'
+            ),
         )
         ordering = ('-date_joined', )
 
 
 Group.is_county_level = property(
     lambda self: 'county_group_marker' in [
+        perm.codename for perm in self.permissions.all()]
+)
+
+Group.superusers = property(
+    lambda self: 'manipulate_superusers' in [
         perm.codename for perm in self.permissions.all()]
 )
 
