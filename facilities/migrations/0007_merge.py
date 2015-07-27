9c3c14e6f7e7a5841810bf2df3f5670bd9fee975
bmarika <marika@savannahinformatics.com>
2015-07-27 14:36:17 +0300

    Fix migrations

---
+++ facilities/migrations/0007_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('facilities', '0005_auto_20150724_1359'),
+        ('facilities', '0006_auto_20150723_1153'),
+    ]
+
+    operations = [
+    ]
