069e8501e9349c94ad64bdd84727717e94822b7b
bmarika <marika@savannahinformatics.com>
2015-08-11 17:06:51 +0300

    Update facility migrations

---
+++ facilities/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('facilities', '0002_auto_20150811_1336'),
+        ('facilities', 'set_facility_code_sequence_min_value'),
+    ]
+
+    operations = [
+    ]
