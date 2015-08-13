ac0967617b188f7045649a544957c9ae39db63db
bmarika <marika@savannahinformatics.com>
2015-08-13 17:39:32 +0300

    Add facility by keph level, by facility type and by constituencies reports.

--- reporting/facility_reports.py
+++ reporting/facility_reports.py
@@ -1,6 +1,7 @@
 from django.apps import apps
 
-from facilities.models import Facility
+from facilities.models import Facility, FacilityType, KephLevel
+from common.models import County, Constituency
 from rest_framework.views import APIView, Response
 
 from .report_config import REPORTS
@@ -43,13 +44,39 @@ class FilterReportMixin(object):
             model, more_filters[0], more_filters[1])
         return model_instances
 
+    def _get_return_data(
+            self, filter_field_name, model_instances, return_instance_name,
+            return_count_name):
+        data = []
+
+        for instance in model_instances:
+            filiter_data = {
+                filter_field_name: instance
+            }
+            count = self.queryset.filter(**filiter_data).count()
+            instance_name = instance.name
+            data.append(
+                {
+                    return_instance_name: instance_name,
+                    return_count_name: count
+                }
+            )
+        return data
+
     def get_report_data(self, *args, **kwargs):
         report_type = self.request.query_params.get(
             "report_type", "facility_count_by_county")
+        if report_type == "facility_count_by_facility_type_detailed":
+            return self._get_facility_type_data()
+        if report_type == "facility_keph_level_report":
+            return self._get_facility_keph_level_data()
+        if report_type == "facility_constituency_report":
+            return self._get_facility_constituency_data()
 
         more_filters_params = self.request.query_params.get("filters", None)
 
         report_config = REPORTS[report_type]
+        group_by = report_config.get("group_by")
         app_label, model_name = report_config.get(
             "filter_fields").get("model").split('.')
         filter_field_name = report_config.get(
@@ -65,27 +92,115 @@ class FilterReportMixin(object):
             "filter_fields").get("return_field")[0]
         return_count_name = report_config.get(
             "filter_fields").get("return_field")[1]
+        if group_by:
+            pass
+        else:
+            data = self._get_return_data(
+                filter_field_name, model_instances, return_instance_name,
+                return_count_name)
+        return data, self.queryset.count()
+
+    def _get_facility_type_data(self):
+        owner_cagegory = self.request.query_params.get("owner_category")
+        county = self.request.query_params.get("county")
+        facility_type = self.request.query_params.get("facility_type")
+
         data = []
 
-        for instance in model_instances:
-            filiter_data = {
-                filter_field_name: instance
+        for county in County.objects.all():
+            county_data = {
+                "county": county.name,
+                "facilities": []
             }
-            count = self.queryset.filter(**filiter_data).count()
-            instance_name = instance. name
-            data.append(
-                {
-                    return_instance_name: instance_name,
-                    return_count_name: count
-                }
-            )
-        return {
-            "results": data,
-            "total": self.queryset.count()
-        }
+            for facility_type in FacilityType.objects.all():
+                if not owner_cagegory:
+                    count = Facility.objects.filter(
+                        facility_type=facility_type,
+                        ward__constituency__county=county).count()
+                else:
+                    count = Facility.objects.filter(
+                        facility_type=facility_type,
+                        ward__constituency__county=county,
+                        owner__owner_type=owner_cagegory).count()
+
+                county_data["facilities"].append(
+                    {
+                        "facility_type": facility_type.name,
+                        "number_of_facilities": count
+                    }
+                )
+            data.append(county_data)
+        totals = []
+
+        return data, totals
+
+    def _get_facility_keph_level_data(self):
+        owner_cagegory = self.request.query_params.get("owner_category")
+        county = self.request.query_params.get("county")
+
+        data = []
+
+        for county in County.objects.all():
+            county_data = {
+                "county": county.name,
+                "facilities": []
+            }
+            for level in KephLevel.objects.all():
+
+                if not owner_cagegory:
+                    count = Facility.objects.filter(
+                        keph_level=level,
+                        ward__constituency__county=county).count()
+                else:
+                    count = Facility.objects.filter(
+                        level=level,
+                        ward__constituency__county=county,
+                        owner__owner_type=owner_cagegory).count()
+
+                county_data["facilities"].append(
+                    {
+                        "keph_level": level.name,
+                        "number_of_facilities": count
+                    }
+                )
+            data.append(county_data)
+        return data, 0
+
+    def _get_facility_constituency_data(self):
+        owner_cagegory = self.request.query_params.get("owner_category")
+        county = self.request.query_params.get("county")
+
+        data = []
+
+        for county in County.objects.all():
+            county_data = {
+                "county": county.name,
+                "facilities": []
+            }
+            for const in Constituency.objects.filter(county=county):
+                if not owner_cagegory:
+                    count = Facility.objects.filter(
+                        ward__constituency=const).count()
+                else:
+                    count = Facility.objects.filter(
+                        ward__constituency=const,
+                        owner__owner_type=owner_cagegory).count()
+
+                county_data["facilities"].append(
+                    {
+                        "constituency": const.name,
+                        "number_of_facilities": count
+                    }
+                )
+            data.append(county_data)
+        return data, 0
 
 
 class ReportView(FilterReportMixin, APIView):
     def get(self, *args, **kwargs):
-        data = self.get_report_data()
-        return Response(data=data)
+        data, totals = self.get_report_data()
+
+        return Response(data={
+            "results": data,
+            "total": totals
+        })
--- reporting/report_config.py
+++ reporting/report_config.py
@@ -1,7 +1,11 @@
+"""Module holding the configuration of various reports"""
+
+
 REPORTS = {
 
     # facility county by counties
     "facility_count_by_county": {
+        "type": "simple",
         "filter_fields": {
             "model": "common.County",
             "filter_field_name": "ward__constituency__county",
@@ -12,6 +16,7 @@ REPORTS = {
 
     # facility count by constituencies
     "facility_count_by_consituency": {
+        "type": "simple",
         "filter_fields": {
             "model": "common.Constituency",
             "filter_field_name": "ward__constituency",
@@ -29,6 +34,7 @@ REPORTS = {
 
     # facility count by owner categories
     "facility_count_by_owner_category": {
+        "type": "simple",
         "filter_fields": {
             "model": "facilities.OwnerType",
             "filter_field_name": "owner__owner_type",
@@ -39,6 +45,7 @@ REPORTS = {
 
     # facility count by owners
     "facility_count_by_owner": {
+        "type": "simple",
         "filter_fields": {
             "model": "facilities.Owner",
             "filter_field_name": "owner",
@@ -56,10 +63,47 @@ REPORTS = {
 
     # facility county by facility types
     "facility_count_by_facility_type": {
+        "type": "simple",
         "filter_fields": {
             "model": "facilities.FacilityType",
             "filter_field_name": "facility_type",
             "return_field": ["type_category", "number_of_facilities"]
+        },
+        "extra_filters": {
+            "owner_type": {
+                "path": "facilities.OwnerType",
+                "filter_field_name": "owner__owner_type"
+            }
+        },
+        "top_level_field": "total"
+    },
+    "facility_count_by_facility_type_detailed": {
+        "type": "complex",
+        "filter_fields": {
+            "model": "facilities.FacilityType",
+            "filter_field_name": "facility_type",
+            "return_field": ["type_category", "number_of_facilities"]
+        },
+        "extra_filters": {
+            "owner_type": {
+                "path": "facilities.OwnerType",
+                "filter_field_name": "owner__owner_type"
+            }
+        },
+        "facility_fields": [
+            {
+                "name": "county",
+                "field_name": "ward.constituency.county.name"
+            },
+            {
+                "name": "owner_category",
+                "field_name": "owner.owner_type.name"
+            }
+        ],
+        "group_by": {
+            "path": "common.County",
+            "name": "county",
+            "field_name": "ward__constituency__county"
         },
         "top_level_field": "total"
     }
