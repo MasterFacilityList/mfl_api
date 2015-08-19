001a9632d2dc6c48ab1c732596a94985c6bcd8e5
bmarika <marika@savannahinformatics.com>
2015-08-19 14:36:47 +0300

    Add permission for viewing rejected facilities

--- common/migrations/0002_merge.py
+++ common/migrations/0002_merge.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
         ('common', 'county_seq'),
         ('common', 'ward_seq'),
         ('common', 'constituency_code'),
-        ('common', '0002_auto_20150817_0630'),
     ]
 
     operations = [
