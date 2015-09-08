16551f61562a9cf998ea02b8a27e5c34953ac4a3
bmarika <marika@savannahinformatics.com>
2015-09-08 14:24:14 +0300

    Reset sequences for all sequece fields when migrating

--- facilities/migrations/set_facility_code_sequence_min_value.py
+++ facilities/migrations/set_facility_code_sequence_min_value.py
@@ -6,7 +6,9 @@ def set_min_code_value(apps, schema_editor):
     from django.db import connection
     cursor = connection.cursor()
     sql = """
-          ALTER SEQUENCE facilities_facility_code_seq restart 100000 start 100000 minvalue 100000
+          ALTER SEQUENCE facilities_facility_code_seq restart 100000 start 100000 minvalue 100000;
+          ALTER SEQUENCE facilities_owner_code_seq restart 1000 start 1000 minvalue 1000;
+          ALTER SEQUENCE facilities_service_code_seq restart 1000 start 1000 minvalue 1000;
           """
     cursor = cursor.execute(sql)
 
