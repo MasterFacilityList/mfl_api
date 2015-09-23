808de2532e711a2ce7bcccfafc8b4e23fb3da7f7
bmarika <marika@savannahinformatics.com>
2015-09-23 19:19:24 +0300

    Checkpoint

--- chul/serializers.py
+++ chul/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
-from common.serializers import AbstractFieldsMixin
+from common.serializers import AbstractFieldsMixin, ContactSerializer
+from common.models import Contact
 
 from .models import (
     CommunityHealthUnit,
@@ -69,23 +70,57 @@ class CommunityHealthUnitSerializer(
                 data=chew, context=context)
             chew_data.save() if chew_data.is_valid() else None
 
+    def create_contact(self, contact_data):
+
+        try:
+            return Contact.objects.get(contact=contact_data["contact"])
+        except Contact.DoesNotExist:
+            contact = ContactSerializer(
+                data=contact_data, context=self.context)
+            if contact.is_valid():
+                return contact.save()
+            else:
+                self.inlining_errors.update(contact.errors)
+
+    def create_chu_contacts(self, instance, contacts, validated_data):
+        for contact_data in contacts:
+            contact = self.create_contact(contact_data)
+            if contact:
+                health_unit_contact_data_unadit = {
+                    "contact": contact,
+                    "health_unit": instance
+                }
+                chu_contact_data = self.inject_audit_fields(
+                    health_unit_contact_data_unadit, validated_data)
+                try:
+                    CommunityHealthUnitContact.objects.get(
+                        **health_unit_contact_data_unadit)
+                except CommunityHealthUnitContact.DoesNotExist:
+                    CommunityHealthUnitContact.objects.create(
+                        **chu_contact_data)
+
     def create(self, validated_data):
         chews = self.initial_data.pop('health_unit_workers', [])
+        contacts = self.initial_data.pop('contacts', [])
+
         self._validate_chew(chews, self.context)
 
         if not self.inlined_errors:
             chu = super(CommunityHealthUnitSerializer, self).create(
                 validated_data)
             self.save_chew(chu, chews, self.context)
+            self.create_chu_contacts(chu, contacts, validated_data)
             return chu
         else:
             raise ValidationError(self.errors)
 
     def update(self, instance, validated_data):
         chews = self.initial_data.pop('health_unit_workers', [])
+        contacts = self.initial_data.pop('contacts', [])
         super(CommunityHealthUnitSerializer, self).update(
             instance, validated_data)
         self.save_chew(instance, chews, self.context)
+        self.create_chu_contacts(instance, contacts, validated_data)
         return instance
 
 
