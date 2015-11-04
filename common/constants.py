1d2e27248519e3ba2b87ff068799d22e990b42c0
bmarika <marika@savannahinformatics.com>
2015-11-04 15:21:56 +0300

    clean up

--- common/constants.py
+++ common/constants.py
@@ -1,8 +1,7 @@
 """Module to hold constant variable to be used throughout the code base"""
 
 
-
 TRUTH_NESS = ['True', 'TRUE', 'true', 't', 'T', 'Y', 'y', 'yes', 'Yes', 'YES', '1']  # NOQA
 
 FALSE_NESS = ['False', 'FALSE', 'false', 'f', 'F', 'N', 'n', 'no', 'No', 'NO', '0']  # NOQA
-BOOLEAN_CHOICES = tuple((value, 'True') for value in TRUTH_NESS) + tuple((value, 'False') for value in FALSE_NESS) # NOQA
+BOOLEAN_CHOICES = tuple((value, 'True') for value in TRUTH_NESS) + tuple((value, 'False') for value in FALSE_NESS)  # NOQA
