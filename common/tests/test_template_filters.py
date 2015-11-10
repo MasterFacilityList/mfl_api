798a8101535cd31a022607d4cd3317ad22efd8ad
bmarika <marika@savannahinformatics.com>
2015-11-10 11:52:11 +0300

    Add docstrings to tests

--- common/tests/test_template_filters.py
+++ common/tests/test_template_filters.py
@@ -3,8 +3,14 @@ from django.template import Template, Context
 
 
 class TestTemplateTags(TestCase):
+    """
+    Test the custom filters class
+    """
 
     def test_true_filter(self):
+        """
+        Test True is converted to Yes
+        """
         template = (
             "{% load mfl_none_filter %}"
             "{{attribute | mfl_bool_none_date_filter}}"
@@ -17,6 +23,9 @@ class TestTemplateTags(TestCase):
         self.assertEqual(val, "Yes")
 
     def test_false_filter(self):
+        """
+        Test False is converted to No
+        """
         template = (
             "{% load mfl_none_filter %}"
             "{{attribute | mfl_bool_none_date_filter}}"
@@ -29,6 +38,9 @@ class TestTemplateTags(TestCase):
         self.assertEqual(val, "No")
 
     def test_none_filter(self):
+        """
+        Test None is converted to Not Applicable
+        """
         template = (
             "{% load mfl_none_filter %}"
             "{{attribute | mfl_bool_none_date_filter}}"
@@ -41,6 +53,9 @@ class TestTemplateTags(TestCase):
         self.assertEqual(val, "Not Applicable")
 
     def test_datetime_filter(self):
+        """
+        Test Datetime is converted to date only
+        """
         template = (
             "{% load mfl_none_filter %}"
             "{{attribute | mfl_bool_none_date_filter}}"
@@ -53,6 +68,9 @@ class TestTemplateTags(TestCase):
         self.assertEqual(val, "2015 - 11 - 9")
 
     def test_date_filter(self):
+        """
+        Test date is converted to a good human format
+        """
         template = (
             "{% load mfl_none_filter %}"
             "{{attribute | mfl_bool_none_date_filter}}"
@@ -65,6 +83,9 @@ class TestTemplateTags(TestCase):
         self.assertEqual(val, "2015 - 11 - 9")
 
     def test_non_filterable_value(self):
+        """
+        Test anything that is not handled by the filter is retained
+        """
         template = (
             "{% load mfl_none_filter %}"
             "{{attribute | mfl_bool_none_date_filter}}"
