4f2e640177e37f9cfafc12f39a1006f3ad785cac
jason <jason.wanjohi@savannahinformatics.com>
2015-11-14 15:15:38 +0300

    Changed 'lat_long' field to reflect [latitude, longitude ] format

--- facilities/models/facility_models.py
+++ facilities/models/facility_models.py
@@ -949,7 +949,7 @@ class Facility(SequenceMixin, AbstractBase):
     def lat_long(self):
         coords = self.coordinates
         if coords:
-            return [coords.coordinates.x, coords.coordinates.y]
+            return [coords.coordinates.y, coords.coordinates.x]
         return None
 
     def validate_closing_date_supplied_on_close(self):
