a789b4c88eefed6a59faf739ec8f49988a6f58a3
bmarika <marika@savannahinformatics.com>
2015-11-18 14:06:37 +0300

    Refactor user tasks

--- users/tasks.py
+++ users/tasks.py
@@ -39,12 +39,12 @@ def resend_user_signup_emails():
                 obj.delete()
             else:
                 obj.retries = obj.retries + 1
-                if obj.retries > 2:
-                    mail_admins(
-                        subject="Send User Email Error",
-                        message="Sending emails to users on registration is "
-                        "failing. Check the email settings in the environment"
-                    )
                 obj.save()
+            if obj.retries > 2:
+                mail_admins(
+                    subject="Send User Email Error",
+                    message="Sending emails to users on registration is "
+                    "failing. Check the email settings in the environment"
+                )
         except MflUser.DoesNotExist:
             LOGGER.info("The user has been deleted")
