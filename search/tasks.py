e09ee68a5bfe24ae7dc86a999b5c8f16e2efae6c
bmarika <marika@savannahinformatics.com>
2015-11-18 12:41:22 +0300

    Send emails to admins if the indexing failed records or sending emails to users is failing

--- search/tasks.py
+++ search/tasks.py
@@ -1,11 +1,12 @@
 import pydoc
 import logging
 
-from common.models import ErrorQueue
-from search.search_utils import index_instance
-
+from django.core.mail import mail_admins
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
+
+from common.models import ErrorQueue
+from search.search_utils import index_instance
 
 
 LOGGER = logging.getLogger(__name__)
@@ -16,6 +17,10 @@ LOGGER = logging.getLogger(__name__)
     name="try_indexing_failed_records",
     ignore_result=True)
 def retry_indexing():
+    """
+    Indexes the the objects that were not indexed on save
+    """
+
     objects_with_errors = ErrorQueue.objects.filter(
         error_type='SEARCH_INDEXING_ERROR')
     for obj in objects_with_errors:
@@ -32,6 +37,11 @@ def retry_indexing():
                 obj.delete()
             else:
                 obj.retries = obj.retries + 1
+                if obj.retries > 2:
+                    mail_admins(
+                        message="Indexing failed records is failing."
+                        " Please check and ensure elasticsearch is up"
+                    )
                 obj.save()
         except model.DoesNotExist:
             # The related object is already deleted in the database
--- users/tasks.py
+++ users/tasks.py
@@ -1,10 +1,14 @@
 import pydoc
 import logging
 
-from common.models import ErrorQueue
-from users.models import send_email_on_signup, MflUser
+from django.core.mail import mail_admins
+
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
+
+from common.models import ErrorQueue
+from users.models import send_email_on_signup, MflUser
+
 
 LOGGER = logging.getLogger(__name__)
 
@@ -14,6 +18,10 @@ LOGGER = logging.getLogger(__name__)
     name="try_sending_failed_emails",
     ignore_result=True)
 def resend_user_signup_emails():
+    """
+    Resends emails that failed to be sent during user registration
+    """
+
     objects_with_errors = ErrorQueue.objects.filter(
         error_type='SEND_EMAIL_ERROR')
     for obj in objects_with_errors:
@@ -31,6 +39,11 @@ def resend_user_signup_emails():
                 obj.delete()
             else:
                 obj.retries = obj.retries + 1
+                if obj.retries > 2:
+                    mail_admins(
+                        message="Sending emails to users on registration is "
+                        "failing. Check the email settings in the environment"
+                    )
                 obj.save()
         except MflUser.DoesNotExist:
-            LOGGER.info("The user has deleted")
+            LOGGER.info("The user has been deleted")
