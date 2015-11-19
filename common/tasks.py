963964e88a97e7fc9c6b1f739d71607758ce208b
bmarika <marika@savannahinformatics.com>
2015-11-19 16:46:06 +0300

    Back the db everyday at midnight

---
+++ common/tasks.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+"""
+Backup the Mfl database
+
+Creates a backup file for the mfl db, zips and ships
+it to storage at S3 every day at midnight
+"""
+
+from django.core.mail import mail_admins
+from celery.schedules import crontab
+from celery.decorators import periodic_task
+
+from fabfile import backup_mfl_db
+
+
+@periodic_task(
+    run_every=(crontab(minute=0, hour=0)),
+    name="backup_db",
+    ignore_result=True)
+def backup_db():
+    try:
+        backup_mfl_db()
+    except:
+        mail_admins(
+            subject="MFL Database backup Error",
+            message="An error has occurred while taking the MFL database "
+            "backup . Please investigate the issue"
+        )
